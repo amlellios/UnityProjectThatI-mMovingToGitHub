3897abf14c9e4c8f55fd50668f27caaf90fd1e55
amlellios <alexis.lellios@loop.colum.edu>
2018-02-01 15:41:17 -0600

    Newest update
    Trees and lights and shit
    
    :)

---
+++ Assets/Scripts/BeamsOfLight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamsOfLight : MonoBehaviour
+{
+    [SerializeField]
+    int transparency;
+	// Use this for initialization
+	void Start ()
+    {
+        //gameObject.GetComponent<Renderer>().material.color.a = transparency;
+	}
+	
+	
+}
